a6a0ce86a8f0d26411d09c3b8e8d6dae7868b1b4
sawthet <sawthetphyoe28498@gmail.com>
2022-03-20 21:42:26 +0630

    modified update request

--- static/js/main.js
+++ static/js/main.js
@@ -6,15 +6,19 @@ const formatter = new Intl.NumberFormat("en-US", {
 	maximumFractionDigits: 6,
 });
 
-const items = document.querySelectorAll(".items");
-items.forEach(function (el) {
-	change = el.querySelector(".change");
-	if (parseFloat(change.innerHTML) > 0) {
-		change.style.color = "green";
-	} else {
-		change.style.color = "red";
-	}
-});
+const colorChange = () => {
+	const items = document.querySelectorAll(".items");
+	items.forEach(function (el) {
+		change = el.querySelector(".change");
+		if (parseFloat(change.innerHTML) > 0) {
+			change.style.color = "#16a34a";
+		} else {
+			change.style.color = "#dc2626";
+		}
+	});
+};
+
+colorChange();
 
 const update = function () {
 	const template = document.querySelector("main").dataset;
@@ -25,29 +29,22 @@ const update = function () {
 		contentType: "application/json",
 		dataType: "json",
 		success: function (result) {
-			console.log("success");
 			length = Object.keys(result).length;
 			for (let i = 0; i < length; i++) {
 				coin = result[i];
 				row = document.querySelector(`.${coin.symbol}`);
 				if (row) {
-					row.querySelector(".price").innerHTML = formatter.format(coin.price);
-					row.querySelector(".marketcap").innerHTML = `$${coin.market_cap}M`;
-					change = row.querySelector(".change");
-					if (coin.price_change_24h > 0) {
-						change.style.color = "#16a34a";
-						change.innerHTML = "+" + coin.price_change_24h.toFixed(2) + "%";
-					} else {
-						change.style.color = "#dc2626";
-						change.innerHTML = coin.price_change_24h.toFixed(2) + "%";
-					}
+					row.querySelector(".price").innerHTML = coin.price;
+					row.querySelector(".marketcap").innerHTML = coin.market_cap;
+					row.querySelector(".change").innerHTML = coin.price_change_24h;
+					colorChange();
 				}
 			}
 		},
 	});
 };
 
-// setInterval(update, 6000);
+setInterval(update, 6000);
 
 const observeEl = document.querySelector(".observe");
 
